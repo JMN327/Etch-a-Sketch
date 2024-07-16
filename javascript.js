3e850d0e11f9b3c39f56581fcde04857ee4d6827
JMN327 <173676271+JMN327@users.noreply.github.com>
2024-07-16 12:06:53 +0100

    Add prompt for grid size and perform checks for correct values

--- javascript.js
+++ javascript.js
@@ -1,21 +1,36 @@
-console.log("hi")
-
 const container = document.querySelector("#container")
 let containerWidth = container.offsetWidth;
-console.log(containerWidth)
-let subdivs = 4;
 
-populate(subdivs);
+getInput();
+populateContainer(subdivs);
 
-function populate(subdivs){
+
+
+function populateContainer(subdivs){
     const sqrCount = subdivs * subdivs;
     const sqrSize = containerWidth / subdivs;
-    console.log(sqrSize);
     for (let i = 0; i < sqrCount; i++) {
         newSqr = document.createElement("div");
         newSqr.classList.toggle("square");
         newSqr.style.width = sqrSize + "px";
         newSqr.style.height = sqrSize + "px";
         container.appendChild(newSqr)
+    }
+}
+
+function getInput(){
+    do {
+        subdivs = prompt("Please enter the grid size from 1 to 100");
+        subdivs = parseInt(subdivs);
+        if (isNaN(subdivs)) {
+            alert("You did not enter a number, please try again");
+        }
+    } while (isNaN(subdivs));
+    
+    if (subdivs > 100) {
+        subdivs = 100;
+    }
+    if (subdivs < 1) {
+        subdivs = 1;
     }
 }
